4787456dfcd60b902930c6613ba020784eb51379
modoff <modoff@example.com>
2025-12-03 20:08:22 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "../framework/adw_simple.h"
+
+class MyWindow : public AdwSimple::Window {
+public:
+  MyWindow(GtkApplication *app) : AdwSimple::Window(app, "My App") {
+    // Add your UI setup here
+    GtkWidget *group = add_group("Settings");
+    add_row(group, "Option 1", "preferences-system-symbolic");
+
+    GtkWidget *controls = add_control_box();
+    GtkWidget *button = gtk_button_new_with_label("Click Me");
+    gtk_box_append(GTK_BOX(controls), button);
+  }
+};
+
+int main(int argc, char *argv[]) {
+  AdwSimple::App app("com.example.MyApp", [](GtkApplication *app) {
+    MyWindow *window = new MyWindow(app);
+    window->show();
+  });
+  return app.run(argc, argv);
+}
